bfbad76cd8a9dbd2df3c5c86ea6325485741e95b
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 07:35:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,6 +7,11 @@ def nyc_pigeon_organizer(data)
     memo # Return value for the block, becomes the memo in the next go-round
   end
 
-
+  data.reduce({}) do |memo, (key, value)|
+    p memo # First block parameter
+    p key # Second block parameter
+    p value # Second block parameter
+    memo # Return value for the block, becomes the memo in the next go-round
+  end
 
 end
