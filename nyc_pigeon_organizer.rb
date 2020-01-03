47a6fa614817860bd01924a188d005781a7957d3
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 06:47:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   first_reduction = data.reduce({}) do | memo, (key, value) |
     memo[key] = value
+  end
 
     second_reduction = first_reduction.reduce({}) do | memo, (key, value) |
       memo[key] = value
